fd353754641e2afbe59bae02fa1e3fe67dcfc75e
ZTY <me@zty012.de>
2024-08-09 13:17:09 +0800

    :bug: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ export default function App() {
   const buttonsRef = React.useRef<HTMLDivElement | null>(null);
   const [playing, setPlaying] = React.useState(false);
   const [animating, setAnimating] = React.useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [ledText, _setLedText] = React.useState(
     "这是一个非常无聊但因为有趣的人而有趣起来的服务器"
   );
