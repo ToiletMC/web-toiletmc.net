637c6614e2936b6ccb9c71f3a1f70fba1ecc9722
zty012 <me@zty012.de>
2024-09-14 20:06:14 +0800

    :sparkles: v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import Intro from "./components/Intro";
-import IntroLinks from "./components/IntroLinks";
 
 export default function App() {
-  return (
-    <Intro>
-      <IntroLinks />
-    </Intro>
-  );
+  return <Intro></Intro>;
 }
